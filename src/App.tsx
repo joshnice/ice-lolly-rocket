1ce2ebc0e9fcae4f01e3fd97dfaddf2a39262aec
Josh Nice <joshliamnice@gmail.com>
2021-11-15 16:36:53 +0000

    Added a rating factor

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const rating: number = Math.floor(Math.random() * 11);
+
+
   return (
-    <div>Hello Ice Lolly Rocket Fans! The time is 15:19</div>
+    <>
+      <div>Hello Ice Lolly Rocket Fans! The time is 16:36</div>
+      <div>We like ice lolly rockets this much {rating} out of 10!</div>
+    </>
   );
 }
 
