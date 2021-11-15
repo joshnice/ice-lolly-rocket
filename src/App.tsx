63aedd65a6784204b708f2a0f956f91313fd1bd6
Josh Nice <joshliamnice@gmail.com>
2021-11-15 17:33:28 +0000

    Updated with james bound quote

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App() {
     <>
       <div>Hello Ice Lolly Rocket Fans! The time is 16:36</div>
       <div>We like ice lolly rockets this much {rating} out of 10!</div>
+      <div>An opinion on the james bond film: The Sea Viper (Astra 30) an anti air missile, flies at 4.5 MAC, and has a range of 70 miles. So flight time of 1.30 minutes. Not nine, deary deary me</div>
     </>
   );
 }
